35d091daca06f97972ef6efbbd4723de5868f32f
Kim,Baeg-il <bikim@afreecatv.com>
2016-04-14 14:58:46 +0900

    use relative import path

--- twitter_korean/__init__.py
+++ twitter_korean/__init__.py
@@ -1,4 +1,4 @@
 RESOURCES_ROOT = 'com.twitter.penguin.korean/src/main/resources/com/twitter/penguin/korean/util'
 
-from normalizer.KoreanNormalizer import normalize, normalize_coda_n
-from util.KoreanDictionaryProvider import correct_typo
+from .normalizer.KoreanNormalizer import normalize, normalize_coda_n
+from .util.KoreanDictionaryProvider import correct_typo
