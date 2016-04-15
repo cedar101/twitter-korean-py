955d35695e0354e65a3967d1ce5af4dea8b594eb
Kim,Baeg-il <bikim@afreecatv.com>
2016-04-15 18:44:33 +0900

    introduce the command 'setup.py download_jar'

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import setuptools
-setuptools.setup(setup_requires=['pbr'], pbr=True)
+import os
+import sys
+
+from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+cmd = sys.argv[-1]
+if (cmd.startswith('install') or cmd.startswith('bdist') or cmd.startswith('test')
+    or cmd == 'develop'):
+    os.system("python setup.py download_jar")
+
+setup(
+    setup_requires=[
+        'pbr>=1.9',
+        'setuptools>=17.1',
+        'maven-artifact>=0.1.3'
+    ],
+    pbr=True,
+)
--- twitter_korean/__init__.py
+++ twitter_korean/__init__.py
@@ -1,4 +1,16 @@
+# -*- coding: utf-8 -*-
+
 RESOURCES_ROOT = 'com.twitter.penguin.korean/src/main/resources/com/twitter/penguin/korean/util'
+JAR_DIR = 'data/lib'
 
 from .normalizer.KoreanNormalizer import normalize, normalize_coda_n
 from .util.KoreanDictionaryProvider import correct_typo
+
+def tokenize(normalized):
+    raise NotImplementedError
+
+def stem(tokens):
+    raise NotImplementedError
+
+def extractPhrases(tokens, filterSpam=True, enableHashtags=True):
+    raise NotImplementedError
