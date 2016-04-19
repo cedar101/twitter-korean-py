f3e40eede6962aa7d150d069ba20926709f38b84
Kim,Baeg-il <bikim@afreecatv.com>
2016-04-19 16:10:22 +0900

    +    autotranslate(['maven', 'maven.requestor', 'maven.downloader'])

--- twitter_korean/_setup_hooks.py
+++ twitter_korean/_setup_hooks.py
@@ -9,7 +9,7 @@ import setuptools
 import six
 if six.PY3:
     from past import autotranslate
-    autotranslate('maven') #['maven.requestor', 'maven.downloader'])
+    autotranslate(['maven', 'maven.requestor', 'maven.downloader'])
 from maven.artifact import Artifact
 from maven.downloader import Downloader
 
