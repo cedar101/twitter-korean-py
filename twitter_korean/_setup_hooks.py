3e9112ed123bdc9717dde11fa3509bdd9de6025a
Kim,Baeg-il <bikim@afreecatv.com>
2016-04-19 16:48:34 +0900

    fix python3 installation

--- twitter_korean/_setup_hooks.py
+++ twitter_korean/_setup_hooks.py
@@ -7,28 +7,42 @@ from distutils.command.install_data import install_data
 import setuptools
 
 import six
-if six.PY3:
-    from past import autotranslate
-    autotranslate(['maven', 'maven.requestor', 'maven.downloader'])
-from maven.artifact import Artifact
-from maven.downloader import Downloader
-
-#from twitter_korean import JAR_DIR
+# if six.PY3:
+#     from past import autotranslate
+#     autotranslate(['maven', 'maven.requestor', 'maven.downloader'])
 
 module_name = __name__.split('.')[0]
 #JAR_PATH = os.path.join(module_name, JAR_DIR)
-artifact = Artifact(group_id='com.twitter.penguin', artifact_id='korean-text',
-                    version='4.4')
+
+artifact_info = {'group_id': 'com.twitter.penguin', 'artifact_id': 'korean-text',
+                 'version': '4.4'}
 
 def download_jar(cmdobj):
     '''Download and extract twitter-korean-text jar file'''
-    #filename = os.path.join(JAR_PATH, artifact.get_filename())
-    #log.info('[pbr] Downloading twitter-korean-text jar file to {}'.format(filename))
     log.info('[pbr] Downloading twitter-korean-text jar file')
 
-    dl = Downloader()
-    dl.download(artifact) # may raise RequestException or IOError
-    with ZipFile(artifact.get_filename()) as jar:
+    if six.PY2:
+        from maven.artifact import Artifact
+        from maven.downloader import Downloader
+
+        artifact = Artifact(**artifact_info)
+        dl = Downloader()
+        dl.download(artifact) # may raise RequestException or IOError
+        jar_path = artifact.get_filename()
+    else:   # six.PY3
+        # maven-artifact is not compatible with python 3
+        # install and execute mvn
+        os.system(
+            'mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get'
+            ' -Dartifact={group_id}:{artifact_id}:{version}'.format(**artifact_info)
+        )
+        jar_path = os.path.join('~/.m2/repository',
+                                artifact_info.group_id.replace('.', '/'),
+                                artifact_info.artifact_id,
+                                artifact_info.version,
+                                '{artifact_id}-{version}.jar'.format(**artifact_info))
+
+    with ZipFile(jar_path) as jar:
         text_files = (filename for filename in jar.namelist()
                         if filename.endswith('.txt'))
         jar.extractall(module_name, text_files)
