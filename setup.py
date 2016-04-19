7c81b0501417edb79da3f205610d4f7674cdbe6a
Kim,Baeg-il <bikim@afreecatv.com>
2016-04-19 14:33:13 +0900

    simplify setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,6 @@
 from setuptools import setup
 
 setup(
-    setup_requires=[
-        'pbr',
-        'maven-artifact',
-        'six', 'future'
-    ],
-    dependency_links=[
-        'git+https://github.com/hamnis/maven-artifact.git@0.1.4#egg=maven-artifact-0.1.4'
-    ],
+    setup_requires=['pbr'],
     pbr=True,
 )
